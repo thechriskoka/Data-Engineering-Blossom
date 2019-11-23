5fbe12ae817ae11b28eb27bb0deac28f8d798b23
rchriskoka <rchriskoka@gmail.com>
2019-11-23 10:26:45 +0000

    DDL File

--- project3/ddl.sql
+++ project3/ddl.sql
@@ -1,35 +1,54 @@
 create schema stackoverflow_filtered;
 
+SET search_path TO stackoverflow_filtered;
+
 create table results(
-	  user_id text NOT null,
-	  question_id text NOT null,
-	  title text NOT null,
-	  question_body text NOT null,
-	  accepted_answer_id text NOT null,
-	  question_score text NOT null,
-	  view_count text NOT null,
-	  question_comment_count text NOT null,
-	  question_created_at text NOT null,
-	  display_name text NOT null,
-	  reputation text NOT null,
-	  website_url text NOT null,
-	  "location"text NOT null,
-	  about_me text NOT null,
-	  "views" text NOT null,
-	  up_votes text NOT null,
-	  down_votes text NOT null,
-	  image_url text NOT null,
-	  created_at text NOT null,
-	  updated_at text NOT null,
-	  city text NOT null,
-	  country text NOT null,
-	  answer_id text NOT null,
-	  ans_question_id text NOT null,
-	  answer_body text NOT null,
-	  answer_score text NOT NULL,
-	  answer_comment_count text NOT null,
-	  answer_created_at text NOT null
-	
+		question_id text NOT NULL,
+		user_id text NOT NULL PRIMARY KEY,
+		display_name text NULL,
+		reputation text NULL,
+		website_url text NULL,
+		"location" text NULL,
+		about_me text NULL,
+		"views" text NULL,
+		up_votes text NULL,
+		down_votes text NULL,
+		image_url text NULL,
+		user_created_at text NULL,
+		user_updated_at text NULL,
+		city text NULL,
+		country text NULL,
+		title text NULL,
+		question_body text NULL,
+		accepted_answer_id text NULL,
+		question_score text NULL,
+		view_count text NULL,
+		comment_count text NULL,
+		question_created_at text NULL,
+		answer_id text NULL,
+		answer_body text NULL,
+		answer_score text NULL,
+		answer_comment_count text NULL,
+		answer_created_at text null		
+			 
 );
 
+-- creating a btree index on reputation
+create index reputation_index on results;
 
+-- creating a hash index on display name
+create index display_name on results using hash;
+
+-- creating a view
+create view normal_view as 
+		select display_name, city, question_id from results
+			where accepted_answer_id is not null;
+		
+-- creating a materialized view
+create materialized view  materialized_view as 
+		select display_name, city, question_id from results 
+			where accepted_answer_id is not null;
+
+		
+		
+		
